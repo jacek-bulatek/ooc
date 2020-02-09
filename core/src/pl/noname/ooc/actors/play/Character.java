21fd2d2f42b0d260fd2939a2cb54bb363c81e162
Lolo <boelseboob@gmail.com>
2020-02-09 15:18:02 +0100

    Remove unused imports.

--- core/src/pl/noname/ooc/actors/play/Character.java
+++ core/src/pl/noname/ooc/actors/play/Character.java
@@ -1,6 +1,5 @@
 package pl.noname.ooc.actors.play;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
@@ -8,7 +7,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import pl.noname.ooc.Assets;
-import pl.noname.ooc.OOC;
 
 public class Character extends Actor {
     public enum State {
