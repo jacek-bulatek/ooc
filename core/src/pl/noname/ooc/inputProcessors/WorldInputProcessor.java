48b894b73fa1af07fcc3fc1140cbb29fb01a183d
jacek-bulatek <jacek.bulatek@gmail.com>
2020-02-14 07:15:48 +0100

    [#30] Fix player movement when game paused

--- core/src/pl/noname/ooc/inputProcessors/WorldInputProcessor.java
+++ core/src/pl/noname/ooc/inputProcessors/WorldInputProcessor.java
@@ -2,41 +2,140 @@ package pl.noname.ooc.inputProcessors;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-
+import pl.noname.ooc.actors.play.Character;
 import pl.noname.ooc.screens.Play;
 
 public class WorldInputProcessor implements InputProcessor {
-    Play screen;
+    final private Character hero;
+    final private Play screen;
+    private boolean up = false, down = false, left = false, right = false;
+    private boolean changed = false;
+    public WorldInputProcessor(final Character hero, final Play screen) {
+        this.hero = hero;
+        this.screen = screen;
+    }
 
-    public WorldInputProcessor(Play screen) {this.screen = screen;}
+    private void Update() {
+        if(up) {
+            if(left) {
+                hero.setMovement(Character.Direction.NW);
+            } else if(right) {
+                hero.setMovement(Character.Direction.NE);
+            } else {
+                hero.setMovement(Character.Direction.N);
+            }
+        } else if(down) {
+            if(left) {
+                hero.setMovement(Character.Direction.SW);
+            } else if(right) {
+                hero.setMovement(Character.Direction.SE);
+            } else {
+                hero.setMovement(Character.Direction.S);
+            }
+        } else if(left) {
+            hero.setMovement(Character.Direction.W);
+        } else if(right) {
+            hero.setMovement(Character.Direction.E);
+        } else {
+            hero.stopMovement();
+        }
+    }
 
     @Override
     public boolean keyDown(int keycode) {
-        if(keycode == Input.Keys.TAB)
-            screen.setFlag(Play.INVENTORY);
-        else if(keycode == Input.Keys.ESCAPE)
-            screen.setFlag(Play.MENU);
+        changed = false;
+        switch (keycode) {
+            case Input.Keys.UP:
+                up = true;
+                changed = true;
+                break;
+            case Input.Keys.DOWN:
+                down = true;
+                changed = true;
+                break;
+            case Input.Keys.LEFT:
+                left = true;
+                changed = true;
+                break;
+            case Input.Keys.RIGHT:
+                right = true;
+                changed = true;
+                break;
+            case Input.Keys.TAB:
+            	up = down = left = right = false;
+            	changed = true;
+            	screen.setFlag(Play.INVENTORY);
+            	break;
+            case Input.Keys.ESCAPE:
+            	up = down = left = right = false;
+            	changed = true;
+            	screen.setFlag(Play.MENU);
+            	break;
+        }
+        if(changed)
+        {
+            Update();
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean keyUp(int keycode) {return false;}
+    public boolean keyUp(int keycode) {
+        changed = false;
+        switch (keycode) {
+            case Input.Keys.UP:
+                up = false;
+                changed = true;
+                break;
+            case Input.Keys.DOWN:
+                down = false;
+                changed = true;
+                break;
+            case Input.Keys.LEFT:
+                left = false;
+                changed = true;
+                break;
+            case Input.Keys.RIGHT:
+                right = false;
+                changed = true;
+                break;
+        }
+        if(changed)
+        {
+            Update();
+            return true;
+        }
+        return false;
+    }
 
     @Override
-    public boolean keyTyped(char character) {return false;}
+    public boolean keyTyped(char character) {
+        return false;
+    }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {return false;}
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
 
     @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {return false;}
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
 
     @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {return false;}
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
 
     @Override
-    public boolean mouseMoved(int screenX, int screenY) {return false;}
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
 
     @Override
-    public boolean scrolled(int amount) {return false;}
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
