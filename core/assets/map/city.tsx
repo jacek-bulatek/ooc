40332011d8ad41630a115ba083cb893e30938594
jacek-bulatek <jacek.bulatek@gmail.com>
2020-02-15 17:52:10 +0100

    [#45] Change map to orthogonal

---
+++ core/assets/map/city.tsx
@@ -0,0 +1,254 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="city" tilewidth="32" tileheight="32" tilecount="360" columns="8">
+ <image source="magecity.png" width="256" height="1450"/>
+ <tile id="4" type="blocked"/>
+ <tile id="5" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="7" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="15" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20" type="blocked"/>
+ <tile id="24" type="blocked"/>
+ <tile id="27" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="29" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="72" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="73" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="74" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="80" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="82" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="88" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="89" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="113" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="128" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="129" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="145" type="blocked"/>
+ <tile id="192" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="193" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="194" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="197" type="blocked"/>
+ <tile id="198" type="blocked"/>
+ <tile id="199" type="blocked"/>
+ <tile id="200" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="201" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="202" type="blocked">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="205" type="blocked"/>
+ <tile id="206" type="blocked"/>
+ <tile id="207" type="blocked"/>
+ <tile id="212">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="213">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="214">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="220">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="221">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="222">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="230">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
--- core/src/pl/noname/ooc/actors/play/World.java
+++ core/src/pl/noname/ooc/actors/play/World.java
@@ -7,15 +7,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
-import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
@@ -29,7 +24,6 @@ public class World extends Actor {
     private TiledMap map;
     private TiledMapRenderer renderer;
     private OrthographicCamera camera;
-    private final static int[] WALKABLE_TILES = {19, 18, 15, 14, 8, 7, 6, 4, 3, 2, 1};
     private Character hero;
     private Character hero2;
     private WorldInputProcessor inputProcessor;
@@ -39,17 +33,16 @@ public class World extends Actor {
     public World(Play screen) {
     	blockedTiles = new ArrayList<>();
         map = Assets.MAP.get();
-        setupMapObjectsLayer();
         hero = new Character();
-        hero.setPosition(44*64,47);
+        hero.setPosition(CellToPosition(40, 40).x, CellToPosition(40,50).y);
         hero.setWorld(this);
         hero2 = new Character();
-        hero2.setPosition(47*64,47);
+        hero2.setPosition(CellToPosition(60, 20).x, CellToPosition(60,20).y);
         hero2.setWorld(this);
         onMapObjects.addActor(hero);
         onMapObjects.addActor(hero2);
         inputProcessor = new WorldInputProcessor(hero, screen);
-        renderer = new IsometricTiledMapRenderer(map);
+        renderer = new OrthogonalTiledMapRenderer(map);
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         camera = new OrthographicCamera(w,h);
@@ -78,53 +71,50 @@ public class World extends Actor {
     }
 
     public Vector2 PositionToCell(Vector2 pos) {
-        pos.x = (pos.x-10f)/32f;
-        pos.y = (pos.y-10f)/32f+100f;
-        Vector2 cell = i2c(pos);
-        cell.x -= 100f;
+    	Vector2 cell = new Vector2();
+        cell.x = (int) (pos.x / 32f);
+        cell.y = (int) (pos.y / 32f);
         return cell;
+    }
+    
+    public Vector2 CellToPosition(int x, int y) {
+    	Vector2 pos = new Vector2();
+    	pos.x = x * 32;
+    	pos.y = y * 32;
+    	return pos;
     }
 
     public boolean isWalkable(Vector2 pos) {
-        Vector2 isoPos = PositionToCell(pos);
-        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
-        TiledMapTileLayer.Cell cell = layer.getCell(99-(int)isoPos.y, (int)isoPos.x);
-        TiledMapTileLayer layer2 = (TiledMapTileLayer) map.getLayers().get(1);
-        TiledMapTileLayer.Cell cell2 = layer2.getCell(99-(int)isoPos.y, (int)isoPos.x);
+        Vector2 cellPos = PositionToCell(pos);
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(1);
+        TiledMapTileLayer.Cell cell = layer.getCell((int)cellPos.x, (int)cellPos.y);
         
         if(cell != null) {
-            if(isBlocked(cell2.getTile()))
+            if(isBlocked(cell))
             	return false;
-            int tile_id = cell.getTile().getId();
-            boolean flag = false;
-            for(int id : WALKABLE_TILES) {
-                if(tile_id == id) {
-                    flag = true;
-                    break;
-                }
-            }
-            return flag;
+            else
+            	return true;
         }
         return false;
     }
 
     public void setBlocked(Vector2 pos) {
-    	Vector2 isoPos = PositionToCell(pos);
+    	Vector2 cellPos = PositionToCell(pos);
         TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(1);
-        TiledMapTileLayer.Cell cell = layer.getCell(99-(int)isoPos.y, (int)isoPos.x);
-        blockedTiles.add(cell.getTile());
+        TiledMapTileLayer.Cell cell = layer.getCell((int)cellPos.x, (int)cellPos.y);
+        blockedTiles.add(cell);
     	
     }
     
     public void clearBlocked(Vector2 pos) {
-    	Vector2 isoPos = PositionToCell(pos);
+    	Vector2 cellPos = PositionToCell(pos);
         TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(1);
-        TiledMapTileLayer.Cell cell = layer.getCell(99-(int)isoPos.y, (int)isoPos.x);
-        blockedTiles.remove(cell.getTile());
+        TiledMapTileLayer.Cell cell = layer.getCell((int)cellPos.x, (int)cellPos.y);
+        blockedTiles.remove(cell);
     }
     
-    public boolean isBlocked(TiledMapTile tile) {
-    	return blockedTiles.contains(tile);
+    public boolean isBlocked(TiledMapTileLayer.Cell cell) {
+    	return blockedTiles.contains(cell) || cell.getTile().getId() != 100;
     }
     
     public OrthographicCamera getCamera() {
@@ -137,65 +127,4 @@ public class World extends Actor {
         camera.position.y = heroPos.y;
         camera.update();
     }
-
-    static private Vector2 c2i(Vector2 c) {
-        Vector2 i = new Vector2();
-        i.x = c.x-c.y;
-        i.y = (c.x+c.y)/2f;
-        return i;
-    }
-
-    static private  Vector2 i2c(Vector2 i) {
-        Vector2 c = new Vector2();
-        c.x = (i.x + i.y*2f)/2f;
-        c.y = -i.x + c.x;
-        return c;
-    }
-
-    public void DebugTile(int x, int y, ShapeRenderer sr) {
-        float x1 = (x)*32f+10f;
-        float y1 = (y-100)*32f+10f;
-        float x2 = x1+32f;
-        float y2 = y1+32;
-        Vector2 p1 = c2i(new Vector2(x1, y1));
-        Vector2 p2 = c2i(new Vector2(x2, y1));
-        Vector2 p3 = c2i(new Vector2(x2, y2));
-        Vector2 p4 = c2i(new Vector2(x1, y2));
-        sr.line(p1, p2);
-        sr.line(p2, p3);
-        sr.line(p3, p4);
-        sr.line(p4, p1);
-    }
-    
-    private void setupMapObjectsLayer() {
-    	TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(1);
-    	TiledMapTileSet tileSet = new TiledMapTileSet();
-    	int tileId = 0;
-    	for(int row = 0; row < 100; row++) {
-    		for(int col = 0; col < 100; col++) {
-    			TiledMapTile tile = new StaticTiledMapTile(new TextureRegion());
-    			tile.setId(tileId++);
-    			TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
-    			cell.setTile(tile);
-    			layer.setCell(col, row, cell);
-    		}
-    	}
-    }
-/*
- * Func: DrawDebug
- * 
- * Highlights tile that hero is currently on
- * 
-    public void DrawDebug(ShapeRenderer sr) {
-        sr.setProjectionMatrix(camera.combined);
-        sr.begin(ShapeRenderer.ShapeType.Filled);
-
-        float x = (camera.position.x-10f)/32f;
-        float y = (camera.position.y-10f)/32f+100f;
-        Vector2 tn = PositionToCell(new Vector2(camera.position.x , camera.position.y));
-        sr.setColor(Color.LIME);
-        DebugTile((int)Math.floor(tn.x),(int)Math.floor(tn.y), sr);
-        sr.end();
-    }
-*/
 }
