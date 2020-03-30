1045cfa2ab4744b0dcb0bdd238d19c1bb096658a
twakula <59975733+twakula@users.noreply.github.com>
2020-03-30 22:55:30 +0200

    Logo screen, main menu with background (#78)

--- desktop/src/com/packtpub/libgdx/bludbourne/desktop/DesktopLauncher.java
+++ desktop/src/com/packtpub/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -10,10 +10,10 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.title = "BludBourne";
+		config.title = "OOC";
 		config.useGL30 = false;
-		config.width = 800;
-		config.height = 600;
+		config.width = 1280;
+		config.height = 720;
 
 		Application app = new LwjglApplication(new BludBourne(), config);
 
